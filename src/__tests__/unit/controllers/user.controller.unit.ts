201d87c580b4a8297c6dadb9cc069d1935b89aed
Jaranchai Nut <jaranchai.nt@hotmail.com>
2023-08-11 07:13:59 +0700

    Update user.controller.unit.ts

--- src/__tests__/unit/controllers/user.controller.unit.ts
+++ src/__tests__/unit/controllers/user.controller.unit.ts
@@ -1,20 +1,20 @@
-// import {UserController} from '../../../controllers';
-// import {UserDataSource} from '../../../datasources';
-// import {UserRepository} from '../../../repositories';
+import {UserController} from '../../../controllers';
+import {UserDataSource} from '../../../datasources';
+import {UserRepository} from '../../../repositories';
 
-// describe('User Controller Connect Real Database', () => {
-//   let userController: UserController;
+describe('User Controller Connect Real Database', () => {
+  let userController: UserController;
 
-//   beforeEach(() => {
-//     const userDataSource = new UserDataSource();
-//     const userRepo = new UserRepository(userDataSource);
-//     userController = new UserController(userRepo);
-//   });
+  beforeEach(() => {
+    const userDataSource = new UserDataSource();
+    const userRepo = new UserRepository(userDataSource);
+    userController = new UserController(userRepo);
+  });
 
-//   describe('User Controller', () => {
-//     it('Query ข้อมูล User เชื่อมต่อจริง', async () => {
-//       const result = await userController.find();
-//       console.log('result', result);
-//     });
-//   });
-// });
+  describe('User Controller', () => {
+    it('Query ข้อมูล User เชื่อมต่อจริง', async () => {
+      const result = await userController.find();
+      console.log('result', result);
+    });
+  });
+});
